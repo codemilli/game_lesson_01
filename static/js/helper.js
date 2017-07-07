297924ce2b86209932714a9dbc68fa4d04afd636
Jooyoung Moon <hckrmoon@gmail.com>
2017-07-07 17:31:22 +0900

    set

---
+++ static/js/helper.js
@@ -0,0 +1,13 @@
+function createVector() {
+
+}
+
+function Blob() {
+    this.pos = createVector(width / 2, height / 2);
+    this.r = 64;
+}
+
+Blob.prototype.show = function() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+}
---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function setup() {
+    createCanvas(600, 600);
+}
+
+function draw() {
+    background(0);
+}
